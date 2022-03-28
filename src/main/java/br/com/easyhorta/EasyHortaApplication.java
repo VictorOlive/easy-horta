7254cd80294ce473fe7ec958bf29b83c3b9fd69d
Victor <victor_oliveirasilva@hotmail.com>
2022-03-28 18:34:38 -0300

    teste

---
+++ src/main/java/br/com/easyhorta/EasyHortaApplication.java
@@ -0,0 +1,13 @@
+package br.com.easyhorta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyHortaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyHortaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/easyhorta/EasyHortaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.easyhorta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyHortaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
